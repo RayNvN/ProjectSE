cac0606c7a8648c7c51a391a39f20136f9151297
tristanray4 <tristanraynevan@gmail.com>
2024-06-22 16:44:34 +0700

    commit

--- app/src/main/java/com/example/greentransport/HomeFragment.java
+++ app/src/main/java/com/example/greentransport/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 public class HomeFragment extends Fragment {
@@ -14,7 +15,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
 }
--- app/src/main/java/com/example/greentransport/MyDatabaseHelper.java
+++ app/src/main/java/com/example/greentransport/MyDatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.greentransport;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -35,5 +37,20 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Table_name);
         onCreate(db);
+    }
+
+    void addTravel(String currentLocation, String destination, int distance, int cost){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(Column_CurrentLocation, currentLocation);
+        cv.put(Column_Destination, destination);
+        cv.put(Column_Distance, distance);
+        cv.put(Column_Cost, cost);
+        long result = db.insert(Table_name, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Travel Added Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
